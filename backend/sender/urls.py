eb4d6d3046a0cd2fd6219c399bf7cf747c34996e
EhnNoz <enorozzadeh@gmail.com>
2025-06-17 13:09:05 +0330

    1st edition

---
+++ backend/sender/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from . import views
+
+router = DefaultRouter()
+router.register(r'provinces', views.ProvinceViewSet, basename='provinces')
+router.register(r'channels', views.ChannelViewSet, basename='channels')
+router.register(r'send-posts', views.PostViewSet, basename='send-posts')
+router.register(r'category', views.CategoryViewSet, basename='category')
+router.register(r'platform-token', views.PlatformTokenViewSet, basename='platform-token')
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
